0e04e32b7b3b7ae4eb14f548c9f826233a7c4e73
Marcia Zegarra <marcia.gzq@gmail.com>
2019-10-15 21:40:50 -0400

    new routs added

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES 
 var express = require("express");
 var logger = require("morgan");
-var mongojs = require("mongojs");
+var mongoose = require("mongoose");
 
 // Our scraping tools
 var axios = require("axios");
@@ -19,32 +19,20 @@ var PORT = 3000;
 // Configure middleware
 // Use morgan logger for logging requests
 app.use(logger("dev"));
+
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // Make public a static folder
 app.use(express.static("public"));
 
-
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/Scraper-News", { useNewUrlParser: true });
-
-
-// Configuration of package mongojs to the db variable 
-var db = mongojs(databaseUrl, collections);
-
-// Watching out for errors
-db.on("error", function(err) {
-    console.log("Database Error :", err);
-});
+mongoose.connect("mongodb://localhost/Scraper", { useNewUrlParser: true });
 
 
 // Routes
 // First Route ****
-app.get("/", function(req, res) {
-    res.send("Welcome to Scrape");
-});
-
 
 // Route get NEWS 
 app.get("/scrape", function(req, res) {
@@ -53,44 +41,107 @@ app.get("/scrape", function(req, res) {
     var $ = cheerio.load(response.data);
 
         $(".css-1l4spti").each(function(i, element) {
-            var results = {};
+            // Empty object to save results
+            var result = {};
 
-            results.title = $(this)
+            result.title = $(this)
             .children("a")
             .find("h2")
             .text();
+            // console.log(result.title)
 
-            results.link = $(this)
+            result.link = $(this)
             .children("a")
             .attr("href");
+            // console.log(result.link)
+
+            result.image = $(this)
+            .children("a")
+            .find("figure")
+            .attr("itemid");
+            // console.log(result.image)
+
 
         db.Article.create(result)
         .then(function(dbArticle) {
             console.log(dbArticle)
         })
-        .catch(function(dbArticle) {
-            console.log(dbArticles)
+        .catch(function(err) {
+            console.log(err)
          }) 
         })
-    })
+        // Message to user
+        res.send("Scrape Complete");
+    });
 });
 
 
-// app.put? app.update?
-app.delete("/scrape", function(req, res) {
-
-})
-
-// Route get all JSON ****
-app.get("/all", function(req, res) {
-    db.scraped.find(), function(err, found) {
-        if(err) {
-            console.log(err);
+// Route to find the Articles
+app.get("/articles", function(req, res) {
+    db.Article.find({}, function(err, response) {
+        if(err){
+            console.log(err)
         }else{
-            res.json(found);
+            console.log(response);
+            res.send(response);
         }
-    }
+    });
 });
+
+
+// Route to saved Articles
+app.get("/saved", function(req, res) {
+    db.Article.find({
+        // saved:true?
+    })
+    .populated("note")
+    .then(function(dbArticle) {
+        res.json(dbArticle)
+    })
+    .catch(function(err) {
+        res.json(err)
+    });
+});
+
+// app.post ************
+
+
+
+// Route to save or update a note
+app.get("/note/:id", function(req, res) {
+    db.Note.create(req.body)
+        .then(function(dbNote) {
+            return db.Article.findOneAndUpdate( { _id: req.params.id }, { note: dbNote._id }, { new: true } );
+        })
+        .then(function(dbArticle) {
+            res.json(dbArticle);
+        })
+        .catch(function() {
+            res.json(err);
+        });
+});
+
+
+// 
+app.post("/note/:id", function(req, res) {
+    db.Article.remove(
+        {
+            _id: db.Article.ObjectID(req.params.id)
+        },
+        // Check if any error from the db  
+        function(error, removed) {
+            if(error){
+                console.log(error);
+                res.send(error);
+            // if there's no errors, remove the note.
+            }else {
+                console.log(removed);
+                res.send(removed);
+            }
+        }
+    )
+});
+
 
 
 // Start the server
